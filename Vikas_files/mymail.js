cc6c6e22a31ac93456e272dfe226437532347d9f
Vikas Chauhan <vikaschauhan.kec@gmail.com>
2023-06-01 13:48:32 +0530

    Update mymail.js

--- Vikas_files/mymail.js
+++ Vikas_files/mymail.js
@@ -1,3 +1,3 @@
 function abcdefghijklmn(){
-	document.write("<a href=\"mailto: vikaschauhan.kec@gmail.com\">vikaschauhan.kec@gmail.com<\/a> | <a href=\"mailto: phd1701101006@iiti.ac.in\">phd1701101006@iiti.ac.in<\/a> ");
+	document.write("<a href=\"mailto: vikaschauhan.kec@gmail.com\">vikaschauhan.kec@gmail.com<\/a>");
 }
